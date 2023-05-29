6752f6a2471491d229d19b1b7af240e0c90b0bd7
MateiMadalina <116349352+MateiMadalina@users.noreply.github.com>
2023-05-29 13:58:58 +0300

    Add getGuestsForDay functionality

--- src/main/java/com/codecool/ehotel/EHotelBuffetApplication.java
+++ src/main/java/com/codecool/ehotel/EHotelBuffetApplication.java
@@ -2,9 +2,13 @@ package com.codecool.ehotel;
 
 import com.codecool.ehotel.service.guest.GuestService;
 import com.codecool.ehotel.service.guest.GuestServiceImpl;
+import com.codecool.ehotel.model.Guest;
+import com.codecool.ehotel.model.GuestType;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EHotelBuffetApplication {
 
@@ -17,8 +21,49 @@ public class EHotelBuffetApplication {
 
         // Initialize services
 
-
         // Generate guests for the season
+
+        List<Guest> guests = new ArrayList<>();
+        guests = List.of(
+                new Guest("Holly Rock", GuestType.BUSINESS,
+                        LocalDate.of(2022, Month.JUNE, 1),
+                        LocalDate.of(2022, Month.JUNE, 5)),
+                new Guest("Howard Hiller", GuestType.TOURIST,
+                        LocalDate.of(2022, Month.JUNE, 1),
+                        LocalDate.of(2022, Month.JUNE, 7)),
+                new Guest("Ron Wesley", GuestType.KID,
+                        LocalDate.of(2022, Month.JUNE, 3),
+                        LocalDate.of(2022, Month.JUNE, 9)),
+                new Guest("Hermione Granger", GuestType.KID,
+                        LocalDate.of(2022, Month.JUNE, 3),
+                        LocalDate.of(2022, Month.JUNE, 9)),
+                new Guest("Sirius Black", GuestType.TOURIST,
+                        LocalDate.of(2022, Month.JUNE, 4),
+                        LocalDate.of(2022, Month.JUNE, 7)),
+                new Guest("Uncle Vernon", GuestType.BUSINESS,
+                        LocalDate.of(2022, Month.JUNE, 11),
+                        LocalDate.of(2022, Month.JUNE, 17)),
+                new Guest("Bella Swan", GuestType.TOURIST,
+                        LocalDate.of(2022, Month.JUNE, 16),
+                        LocalDate.of(2022, Month.JUNE, 22)),
+                new Guest("Edward Cullen", GuestType.TOURIST,
+                        LocalDate.of(2022, Month.JUNE, 16),
+                        LocalDate.of(2022, Month.JUNE, 22)),
+                new Guest("Wednesday Adams", GuestType.KID,
+                        LocalDate.of(2022, Month.JUNE, 24),
+                        LocalDate.of(2022, Month.JUNE, 29)),
+                new Guest("Enid Sinclair", GuestType.KID,
+                        LocalDate.of(2022, Month.JUNE, 24),
+                        LocalDate.of(2022, Month.JUNE, 30)),
+                new Guest("Doctor Strange", GuestType.BUSINESS,
+                        LocalDate.of(2022, Month.JUNE, 23),
+                        LocalDate.of(2022, Month.JUNE, 28)),
+                new Guest("Tony Stark", GuestType.BUSINESS,
+                        LocalDate.of(2022, Month.JUNE, 23),
+                        LocalDate.of(2022, Month.JUNE, 28))
+        );
+
+        System.out.println(guestService.getGuestsForDay(guests,LocalDate.of(2022, Month.JUNE, 6)));
 
 
         // Run breakfast buffet
--- src/main/java/com/codecool/ehotel/service/guest/GuestServiceImpl.java
+++ src/main/java/com/codecool/ehotel/service/guest/GuestServiceImpl.java
@@ -4,14 +4,13 @@ import com.codecool.ehotel.model.Guest;
 import com.codecool.ehotel.model.GuestType;
 
 import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
 
 public class GuestServiceImpl implements GuestService {
-    private Guest guest;
-
-    private String[] guestNames = {
+    private final String[] guestNames = {
             "Tomas Rutherford", "Samuel Hammond", "George Nelson",
             "Noelle Dobson", "Lily Verne", "Rosa Hunting",
             "Hugh Boston", "Jack Sparrow", "Will Turner", "Rachel Greene" };
@@ -21,12 +20,8 @@ public class GuestServiceImpl implements GuestService {
         Random rand = new Random();
         String guestName = guestNames[rand.nextInt(guestNames.length)];
         GuestType guestType = GuestType.values()[rand.nextInt(GuestType.values().length)];
-        int maxPeriod = seasonEnd.getDayOfMonth() - seasonStart.getDayOfMonth();
 
-        if (maxPeriod > 7) {
-            maxPeriod = 7;
-        }
-
+        int maxPeriod = Math.min(seasonEnd.getDayOfMonth() - seasonStart.getDayOfMonth(), 7);
         int period = rand.nextInt(maxPeriod) + 1;
         int firstDay = rand.nextInt((seasonStart.getDayOfMonth() - 1), seasonEnd.getDayOfMonth() - period) + 1;
         int lastDay = firstDay + period;
@@ -39,6 +34,12 @@ public class GuestServiceImpl implements GuestService {
 
     @Override
     public Set<Guest> getGuestsForDay(List<Guest> guests, LocalDate date) {
-        return null;
+        Set<Guest> guestsInInterval = new HashSet<>();
+        for (Guest guest : guests) {
+            if(guest.checkIn().isBefore(date) && guest.checkOut().isAfter(date)){
+                guestsInInterval.add(guest);
+            }
+        }
+        return guestsInInterval;
     }
 }
